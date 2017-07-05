13e29d19451d6ad72ace3eabb3817e2acc37bfd9
Dylan Yokoyama <amayokoy2@gmail.com>
2017-07-05 13:13:40 -0700

    js_ps_01

---
+++ js_ps_01.js
@@ -0,0 +1,13 @@
+// Problem Set 01
+
+//1. 
+100 % 3
+//ans = 1
+
+//2. 
+("blah" + "blah")[6]
+// ans = a, remember that indices start with 0 not 1
+
+//3. 
+"hello".length % "hi\\".length
+//ans = 2, \\ is how you write a \ which means that hi\\ is length 3 not 4
