6269b05b9c978b5af907ddc114baba34cca1bcf6
Dylan Yokoyama <amayokoy2@gmail.com>
2017-07-14 18:26:54 -0700

    7/14/17

--- python/AssetShare.py
+++ python/AssetShare.py
@@ -16,15 +16,34 @@ def print_assets_titlecase():
     assets_titlecase = get_assets_titlecase()
     print(assets_titlecase)
 
-def add_asset(name, asset_type):
+def add_asset(name, asset_type="prop"):
     """Add assets to asset dictionary list"""
     asset = {"name": name, "asset_type": asset_type}
     ASSETS.append(asset)
 
-asset_list = get_assets_titlecase()
+def save_file(asset):
+    try:
+        f = open("assets.txt", "a")
+        f.write(asset + "\n")
+        f.close()
+    except Exception:
+        print("Could not save file")
+
+def read_file():
+    try:
+        f = open("assets.txt", "r")
+        for asset in f.readlines():
+            add_asset(asset)
+        f.close()
+    except Exception:
+        print ("Could not read file")
+
+
+read_file()
+print_assets_titlecase()
 
 asset_name = input("Enter asset name: ")
 asset_type = input("Enter asset type: ")
 
 add_asset(asset_name, asset_type)
-print_assets_titlecase()
+save_file(asset_name)
